d58424f4c464242013a5f6cfbfaef72a3177ac6c
So Yeon Kim <ssoykmm@gmail.com>
2023-04-30 20:19:40 +0900

    index() 시간 초과, dict 활용 시간 복잡도

--- PG/LV_1/running_race.py
+++ PG/LV_1/running_race.py
@@ -1,3 +1,7 @@
+# 2 ≤ callings의 길이 ≤ 1,000,000
+# 5 ≤ players의 길이 ≤ 50,000
+
+
 # def solution(players, callings):
 #     answer = players[:]
 #     for c in callings:
@@ -5,7 +9,9 @@
 #         answer[before_idx], answer[before_idx-1] = answer[before_idx-1], answer[before_idx]
 #     return answer
 
-# => .index로 players 배열의 처음부터 탐색
+# => index()로 players 배열의 처음부터 탐색
+# O(nm) = O(50,000 * 1,000,000) = O(50,000,000,000)
+# 500억번의 계산 필요
 
 def solution(players, callings):
     dic = {p:i for i, p in enumerate(players)}
@@ -18,5 +24,8 @@ def solution(players, callings):
         
     return players
 
-# => dict를 사용해 idx를 찾는 시간 단축
-     
+# => dict를 사용해 idx를 찾는 시간 단축]
+# O(m) = O(1,000,000)
+# 100만번의 계산 필요
+
+# 1초에 1억번 이내 -> 두번째 solution 사용
