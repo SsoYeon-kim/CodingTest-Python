c80c6515297e526b80148106ea96f2574894a4ed
Kim So Yeon <62587484+SsoYeon-kim@users.noreply.github.com>
2023-05-08 08:57:54 +0900

    호텔 대실 : 시간은 분으로 바꿔서 풀기

--- PG/LV_2/hotel_room.py
+++ PG/LV_2/hotel_room.py
@@ -6,7 +6,7 @@ def solution(book_time):
         start = int(time[0][:2]) * 60 + int(time[0][3:])
         end = int(time[1][:2]) * 60 + int(time[1][3:]) + 10
 
-        # 930 퇴실일 경우 (for문은 920까지이므로 930 입실 가능)
+        # 930 퇴실일 경우 (for문은 929까지이므로 930 입실 가능)
         for i in range(start, end):
             if dic.get(i) == None:
                 dic[i] = 1
