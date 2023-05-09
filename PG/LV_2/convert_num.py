b85dab314705fbc1b503476d8fdb83000bf537bb
Kim So Yeon <62587484+SsoYeon-kim@users.noreply.github.com>
2023-05-10 00:07:34 +0900

    숫자 변환하기 : dp사용 (테이블, 시작숫자 초기화, 테이블 값 갱신)

--- PG/LV_2/convert_num.py
+++ PG/LV_2/convert_num.py
@@ -1,25 +1,25 @@
 def solution(x, y, n):
     # dp table 초기화
-    dp = [1e9]*(y+1)
+    dp = [1e9]* (y+1)
     # dp[x] = x가 x로 되는 최소 연산횟수 = 0 초기화
     dp[x] = 0
 
-    for i in range(x, y+1):
+    # x부터 목표숫자 y까지
+    for i in range(x,y+1):
         # x에서부터 연산가능한 숫자들만 보기 위함
         if dp[i] == 1e9:
             continue
-        if i + n <= y:
+        # 저장된 값과 i번째에서 연산한 횟수와 비교
+        if i+n <= y:
             dp[i+n] = min(dp[i+n], dp[i]+1)
-        if i * 2 <= y:
+        if i*2 <= y:
             dp[i*2] = min(dp[i*2], dp[i]+1)
-        if i * 3 <= y:
+        if i*3 <= y:
             dp[i*3] = min(dp[i*3], dp[i]+1)
     
     if dp[y] == 1e9:
         return -1
     return dp[y]
 
-
 print(solution(10,40,5))
 # 2
-
